a3629f63a51246205f42c281889f64819f1326f2
Evandro Campigoto <campigoto@hotmail.com>
2022-04-21 20:00:14 -0300

    Desafio concluido !

--- frontweb/src/components/ReviewListing/index.tsx
+++ frontweb/src/components/ReviewListing/index.tsx
@@ -1,28 +1,29 @@
-import { Review } from "types/review";
-
+import { Review } from 'types/review';
 
 import './styles.css';
 
-
-
 type ListingReviews = {
-    reviews: Review[];
-  };
-  
-  const ReviewListing = ({ reviews }: ListingReviews) => {
-  
-    return (
-      <div className="reviews-container">
-        {reviews?.map(review =>(
-          <div className="content-reviews" key={review.id}>
-            <h3>{review.user.name}</h3>
-            <div className="review">
-              <p>{review.text}</p>
+  reviews: Review[];
+};
+
+const ReviewListing = ({ reviews }: ListingReviews) => {
+  return (
+    <div className="reviews-container">
+      {reviews?.map((review) => (
+        <div className="content-reviews" key={review.id}>
+          <div className="listing-review">
+            <div className="starImage">
+            <img  src="" alt='' id="starImageId"></img>
             </div>
+            <h3>{review.user.name}</h3>
           </div>
-        ))}
-      </div>
-    );
-  };
-  
-  export default ReviewListing;
+          <div className="review">
+            <p>{review.text}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ReviewListing;
